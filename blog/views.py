36cdf2f65b44371ff5e38fc7b43f354184ef5959
Noemi <nunesnoemi17@gmail.com>
2018-08-30 20:17:09 -0300

    Adicionando nova pagina abrindo o post

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
+from django.shortcuts import get_object_or_404
 
 
 def post_list(request):
@@ -8,6 +9,8 @@ def post_list(request):
         .order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts':posts})
 
-
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
 
 
